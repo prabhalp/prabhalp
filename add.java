3c8c46d6f8929c8c88567f2f5b13574acb621ce9
prabhalp <50788780+prabhalp@users.noreply.github.com>
2019-06-03 11:13:48 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class add
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter first number:");
+int firstNumber=s.nextInt();
+System.out.println("enter second number:");
+int secondNumber=s.nextInt();
+int sum=firstNumber+secondNumber;
+System.out.println(sum);
+}
+}
+
