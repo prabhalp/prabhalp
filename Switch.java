031cf03406654372a0f9ea2a05a0681c8a539c09
prabhalp <50788780+prabhalp@users.noreply.github.com>
2019-06-03 11:31:03 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,28 @@
+class Switch
+{
+public static void main(String args[])
+{
+char value='k';
+switch(value)
+{
+case 'a':
+System.out.println("vowel");
+break;
+case 'e':
+System.out.println("vowel");
+break;
+case'i':
+System.out.println("vowel");
+break;
+case'o':
+System.out.println("vowel");
+break;
+case'u':
+System.out.println("vowel");
+break;
+default:
+System.out.println("consonant");
+break;
+}
+}
+}
