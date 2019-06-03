d6473adfd936c5e0ac1740c7790eca2f763ca536
prabhalp <50788780+prabhalp@users.noreply.github.com>
2019-06-03 11:30:34 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+class Swap
+{
+public static void main(String args[])
+{
+int num1=4,num2=11,num3=17,num4=20,temp;
+temp=num4;
+num3=num2;
+num2=num1;
+num1=temp;
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+System.out.println(num4);
+}
+}
